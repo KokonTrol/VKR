3bd2d77bfe6bb32c1f96e7a65cece51fe9e6ed32
Жданова Анастасия Александровна <prostonastya01@mail.ru>
2024-03-18 23:46:52 +0500

    change foreign keys in models

--- FlaskSite/app/models.py
+++ FlaskSite/app/models.py
@@ -10,19 +10,20 @@ class Results(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     succes_done = db.Column(db.Boolean, default=False)
     score = db.Column(db.Float, default=0.0)
-
+    comands_id = db.Column(db.Integer, db.ForeignKey('comands.id'), nullable=False)
+    comands = db.relationship('Comands', backref=db.backref('results', lazy=True))
+    subject_id = db.Column(db.Integer, db.ForeignKey('subject.id'), nullable=False)
+    subject = db.relationship('Subject', backref=db.backref('results', lazy=True))
+    
 class Comands(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
 
 class Scores(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    subject_id = db.Column(db.Integer, db.ForeignKey('subject.id'), nullable=False)
-    subject = db.relationship('Subject', backref=db.backref('scores', lazy=True))
     result_id = db.Column(db.Integer, db.ForeignKey('results.id'), nullable=False)
     result = db.relationship('Results', backref=db.backref('scores', lazy=True))
-    comands_id = db.Column(db.Integer, db.ForeignKey('comands.id'), nullable=False)
-    comands = db.relationship('Comands', backref=db.backref('scores', lazy=True))
+    
     visiting = db.Column(db.Float, default=0.0)
     scores_before = db.Column(db.Float, default=0.0)
     scores_test = db.Column(db.Float, default=0.0)
